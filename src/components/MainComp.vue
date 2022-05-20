c0f20d7a2718deb004af6bc0536e2bf187c3e192
carlo-ddm <carloduminuco90@gmail.com>
2022-05-20 11:31:04 +0200

    installazione: axios + bootstrap / organizzazione in componenti e sottocomponenti del componente main

---
+++ src/components/MainComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <MovieScomp />
+    <TvScomp />
+  </main>
+</template>
+
+<script>
+import MovieScomp from './sottocomponenti-main/MovieScomp.vue';
+import TvScomp from './sottocomponenti-main/TvScomp.vue';
+export default {
+    name: "MainComp",
+    components: { MovieScomp, TvScomp }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/sottocomponenti-main/TvScomp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TvScomp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
